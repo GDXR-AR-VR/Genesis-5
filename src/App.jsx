6a4ff5131c33b0074b1d78cf1081274c476b575e
Aditya Pandey <pandeyjiaditya.game@gmail.com>
2026-01-08 21:40:38 +0530

    2

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import HomePage from "./components/HomePage";
+import LoadingPage from "./components/LoadingPage";
+import AboutPage from "./pages/AboutPage";
+import TimelinePage from "./pages/TimelinePage";
+import PrizePoolPage from "./pages/PrizePoolPage";
+import MemoriesPage from "./pages/MemoriesPage";
+import "./App.css";
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadComplete = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <Router>
+      <div className="app">
+        {isLoading ? (
+          <LoadingPage onLoadComplete={handleLoadComplete} />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/timeline" element={<TimelinePage />} />
+            <Route path="/prize-pool" element={<PrizePoolPage />} />
+            <Route path="/memories" element={<MemoriesPage />} />
+          </Routes>
+        )}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
